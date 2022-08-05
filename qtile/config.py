e3c220943583bde41d53b48659d91e5564498379
Shashwat <shashwatagrawal473@gmail.com>
2022-08-05 23:57:30 +0530

    Added Dracula colorscheme

--- qtile/config.py
+++ qtile/config.py
@@ -33,17 +33,17 @@ import os
 import re
 import socket
 import subprocess
-from libqtile import qtile 
+from libqtile import qtile
 from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 from libqtile.widget import Spacer, Backlight
-from libqtile.widget.image import Image 
+from libqtile.widget.image import Image
 
 
 mod = "mod1"
-myTerminal = "qterminal" # guess_terminal()
+myTerminal = "qterminal"  # guess_terminal()
 Primary_Menu = "dmenu_run"
 
 ################################################################
@@ -58,17 +58,22 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([mod], "space", lazy.layout.next(),
+        desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -90,7 +95,7 @@ keys = [
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 
-	# MANUALLY
+    # MANUALLY
 
 
     Key([mod], "p", lazy.spawn(Primary_Menu), desc="Launch Dmenu"),
@@ -101,18 +106,32 @@ keys = [
 ########################      COLORS      ######################
 ################################################################
 
-colors = [["#2B3339", "#2B3339"],  #background (dark grey) [0]
-          ["#7C8377", "#7C8377"],  #light grey [1]
-          ["#D5C9AB", "#D5C9AB"],  #forground (beige) [2]
-          ["#6272a4", "#6272a4"],  #blue/grey) [3]
-          ["#7FBBB3", "#7FBBB3"],  #blue [4]
-          ["#A7C080", "#A7C080"],  #green [5]
-          ["#E69875", "#E69875"],  #orange [6]
-          ["#D196B3", "#D196B3"],  #pink [7]
-          ["#A7C080", "#A7C080"],  #green [8]
-          ['#ED8082', '#ED8080'],  #red [9]
-          ["#D5C9AB", "#D5C9AB"]]  #beige [10]
+# Dracula
 
+colors = [["#282a36", "#282a36"],  # background (dark grey) [0]
+          ["#44475a", "#44475a"],  # light grey [1]
+          ["#f8f8f2", "#f8f8f2"],  # forground (white) [2]
+          ["#6272a4", "#6272a4"],  # blue/grey) [3]
+          ["#8be9fd", "#8be9fd"],  # cyan [4]
+          ["#50fa7b", "#50fa7b"],  # green [5]
+          ["#ffb86c", "#ffb86c"],  # orange [6]
+          ["#ff79c6", "#ff79c6"],  # pink [7]
+          ["#bd93f9", "#bd93f9"],  # purple [8]
+          ['#ff5555', '#ff5555'],  # red [9]
+          ["#f1fa8c", "#f1fa8c"]]  # yellow [10]
+
+
+# colors = [["#2B3339", "#2B3339"],  #background (dark grey) [0]
+# ["#7C8377", "#7C8377"],  #light grey [1]
+# ["#D5C9AB", "#D5C9AB"],  #forground (beige) [2]
+# ["#6272a4", "#6272a4"],  #blue/grey) [3]
+# ["#7FBBB3", "#7FBBB3"],  #blue [4]
+# ["#A7C080", "#A7C080"],  #green [5]
+# ["#E69875", "#E69875"],  #orange [6]
+# ["#D196B3", "#D196B3"],  #pink [7]
+# ["#A7C080", "#A7C080"],  #green [8]
+# ['#ED8082', '#ED8080'],  #red [9]
+# ["#D5C9AB", "#D5C9AB"]]  #beige [10]
 
 
 ################################################################
@@ -136,7 +155,8 @@ for i in groups:
                 [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
+                desc="Switch to & move focused window to group {}".format(
+                    i.name),
             ),
             # Or, use below if you prefer not to switch to that group.
             # # mod1 + shift + letter of group = move focused window to group
@@ -151,13 +171,17 @@ for i in groups:
 ################################################################
 
 layouts = [
-    layout.Columns(border_focus = colors[3], margin = 2),
-	layout.Max(),
+    layout.MonadTall(border_focus=colors[3], margin=5),
+    layout.Max(),
+
+    #layout.Columns(border_focus = colors[3], margin = 5),
+    # layout.Max(),
     #layout.Bsp(border_focus = colors[3], margin = 2),
     #layout.RatioTile(border_focus = colors[3], margin = 2),
     #layout.Tile(border_focus = colors[3], margin = 2),
-	#layout.MonadTall(border_focus = colors[3], margin = 2),
-	#layout.MonadWide(border_focus = colors[3], margin = 2),
+    # layout.Max()
+    #layout.MonadTall(border_focus = colors[3], margin = 2),
+    #layout.MonadWide(border_focus = colors[3], margin = 2),
     # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
     # layout.Max(),
     # Try more layouts by unleashing below layouts.
@@ -174,12 +198,11 @@ layouts = [
 ]
 
 
-
 widget_defaults = dict(
     font="Ubuntu Bold",
     fontsize=12,
     padding=2,
-	background = colors[0],
+    background=colors[0],
 )
 extension_defaults = widget_defaults.copy()
 
@@ -192,38 +215,39 @@ screens = [
     Screen(
         top=bar.Bar(
             [
-				widget.Image(
-					filename = '~/.config/qtile/icon/python.png', 
-					scale = 'False', 
-					margin_x = 5, 
-					mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(Primary_Menu)}
-				), 
+                widget.Image(
+                    filename='~/.config/qtile/icon/python.png',
+                    scale='False',
+                    margin_x=5,
+                    mouse_callbacks={
+                        'Button1': lambda: qtile.cmd_spawn(Primary_Menu)}
+                ),
 
-				widget.Sep(
-					linewidth = 2, 
-					padding =  5, 
-					foreground = colors[2]
-				),  
+                widget.Sep(
+                    linewidth=2,
+                    padding=5,
+                    foreground=colors[2]
+                ),
 
                 widget.GroupBox(
-					margin_x = 5, 
-					active = colors[2], 
-					inactive = colors[1], 
-					highlight_color = ["#2B3339", "A7C080"], 
-					highlight_method = 'line',
-				),
+                    margin_x=5,
+                    active=colors[2],
+                    inactive=colors[1],
+                    highlight_color=["#2B3339", "A7C080"],
+                    highlight_method='line',
+                ),
 
                 widget.Prompt(),
 
-				widget.Sep(
-					linewidth = 2, 
-					padding =  5, 
-					foreground = colors[2]
-				),  
+                widget.Sep(
+                    linewidth=2,
+                    padding=5,
+                    foreground=colors[2]
+                ),
 
                 widget.WindowName(
-					foreground = colors[5]
-				),
+                    foreground=colors[5]
+                ),
 
                 widget.Chord(
                     chords_colors={
@@ -233,70 +257,73 @@ screens = [
                 ),
                 widget.Systray(),
 
-				widget.Sep(
-					linewidth = 2, 
-					padding =  5, 
-					foreground = colors[2]
-				),  
-				
-				widget.Net(
-					interface = 'wlan0', 
-					format = 'Net: {down} ↓↑ {up}', 
-					padding = 5, 
-					foreground = colors[7], 
-                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerminal + ' -e nmtui')},
-				), 
+                widget.Sep(
+                    linewidth=2,
+                    padding=5,
+                    foreground=colors[2]
+                ),
 
-				widget.Sep(
-					linewidth = 2, 
-					padding =  5, 
-					foreground = colors[2]
-				),  
-	
-				widget.CPU(
-			    format = ' {freq_current}GHz {load_percent}%',
-                padding = 10,
-                foreground = colors[10], 
-                mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerminal + ' -e htop')},
-				),
+                widget.Net(
+                    interface='wlan0',
+                    format=' {down} ↓↑ {up}',
+                    padding=5,
+                    foreground=colors[7],
+                    mouse_callbacks={'Button1': lambda: qtile.cmd_spawn(
+                        myTerminal + ' -e nmtui')},
+                ),
 
-				widget.Sep(
-					linewidth = 2, 
-					padding =  5, 
-					foreground = colors[2]
-				),  
+                widget.Sep(
+                    linewidth=2,
+                    padding=5,
+                    foreground=colors[2]
+                ),
 
-				widget.Memory(
-                    foreground = colors[4],
-                    fmt = '  {}',
-                    padding = 10, 
-                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerminal + ' -e htop')},
+                widget.CPU(
+                    format=' {freq_current}GHz {load_percent}%',
+                    padding=10,
+                    foreground=colors[10],
+                    mouse_callbacks={'Button1': lambda: qtile.cmd_spawn(
+                        myTerminal + ' -e htop')},
+                ),
 
-				),
+                widget.Sep(
+                    linewidth=2,
+                    padding=5,
+                    foreground=colors[2]
+                ),
 
-				widget.Sep(
-					linewidth = 2, 
-					padding =  5, 
-					foreground = '#ffffff'
-				),  
+                widget.Memory(
+                    foreground=colors[4],
+                    fmt=' {}',
+                    padding=10,
+                    mouse_callbacks={'Button1': lambda: qtile.cmd_spawn(
+                        myTerminal + ' -e htop')},
+
+                ),
+
+                widget.Sep(
+                    linewidth=2,
+                    padding=5,
+                    foreground='#ffffff'
+                ),
 
                 widget.Clock(
-					format=' %a %d %m %Y |  %I:%M %p', 
-					foreground = colors[2], 
-					padding = 10, 
-				),
+                    format=' %a %d %m %Y |  %I:%M %p',
+                    foreground=colors[8],
+                    padding=10,
+                ),
 
-				widget.Sep(
-					linewidth = 2, 
-					padding =  5, 
-					foreground = '#ffffff'
-				),  
+                widget.Sep(
+                    linewidth=2,
+                    padding=5,
+                    foreground='#ffffff'
+                ),
 
                 widget.QuickExit(
-					fmt = ' ',
-					foreground = colors[9],
-					padding = 10
-				),
+                    fmt=' ',
+                    foreground=colors[9],
+                    padding=10
+                ),
 
             ],
             20,
@@ -308,8 +335,10 @@ screens = [
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
@@ -348,8 +377,8 @@ wl_input_rules = None
 
 @hook.subscribe.startup_once
 def autostart():
-	home = os.path.expanduser('~/.config/qtile/autostart.sh')
-	subprocess.call([home])
+    home = os.path.expanduser('~/.config/qtile/autostart.sh')
+    subprocess.call([home])
 
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
