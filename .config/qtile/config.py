c9dcb6c60d9434d19f87a19b5406373f4f666530
Shashwat <shashwatagrawal473@gmail.com>
2022-08-21 23:27:43 +0530

    Added floating layout border color

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -323,6 +323,7 @@ follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating(
+    border_focus = colors[8],
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
