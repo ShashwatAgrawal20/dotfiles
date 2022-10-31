00a2fe04a8da90223262cb5634283c855641fa25
Shashwat <shashwatagrawal473@gmail.com>
2022-10-31 16:09:26 +0530

    Qtile keybindings, added Screenshot & dmenu height

--- .config/dmenu/config.def.h
+++ .config/dmenu/config.def.h
@@ -20,7 +20,7 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 20;
+static unsigned int lineheight = 43;
 static unsigned int min_lineheight = 8;
 
 /*
