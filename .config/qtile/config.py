835a7bf8033b233521777c34248907cf90f66506
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2023-05-31 12:01:32 +0530

    refactore(qtile): bottom margin for top bar

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -329,7 +329,7 @@ screens = [
             # 20,
             size=36,
             background=backgroundColor,
-            margin=4,
+            margin=[4, 4, 0, 4],
             opacity=0.8,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
