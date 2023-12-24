b41f041d25c0eba61ed005e34e6ea97277d8e410
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2023-12-24 16:21:38 +0530

    refactor(nvim):cmp keymap and telescope search word
     - update the cmp keymap to scroll through docs while in the insert mode
       for consistent half-page scrolling(C-d, C-u)
    
     - replace the crappy 'grep prompt and function' and opted for the native telescope
       'grep_string' function for word searching

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -8,9 +8,7 @@ vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find exist
 
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sw', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
