06eb956b39df8d23d2db39d59ab0ff664cd9c728
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2025-01-01 14:57:19 +0530

    feat(nvim): bringing 2 telescope functionality backkkkk. - we're sooooooo backkkkkkk

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -24,6 +24,7 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
         vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+        vim.keymap.set("n", "<space>/", builtin.current_buffer_fuzzy_find, { desc = '[/] Search in current buffer' })
 
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
         vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
@@ -32,5 +33,10 @@ return {
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set('n', '<leader>fw',
+            function()
+                builtin.grep_string({ search = vim.fn.expand("<cword>") })
+            end,
+            { desc = '[F]ind [W]ord' })
     end
 }
