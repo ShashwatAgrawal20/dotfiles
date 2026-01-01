01dc546221c046a44acefb12b0c1fa27ce2bdd7c
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2026-01-01 21:56:24 +0530

    chore(2026): fix keybinds, no splash
    Signed-off-by: Shashwat Agrawal <shashwatagrawal473@gmail.com>

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,42 +1,37 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    event = 'VeryLazy',
-    branch = '0.1.x',
+    "nvim-telescope/telescope.nvim",
+    event = "VeryLazy",
+    branch = "0.1.x",
     dependencies = {
-        'nvim-lua/plenary.nvim',
+        "nvim-lua/plenary.nvim",
         -- Fuzzy Finder Algorithm which requires local dependencies to be built.
         -- Only load if `make` is available. Make sure you have the system
         -- requirements installed.
         {
-            'nvim-telescope/telescope-fzf-native.nvim',
+            "nvim-telescope/telescope-fzf-native.nvim",
             -- NOTE: If you are having trouble with this installation,
             --       refer to the README for telescope-fzf-native for more instructions.
-            build = 'make',
+            build = "make",
             cond = function()
-                return vim.fn.executable 'make' == 1
+                return vim.fn.executable("make") == 1
             end,
         },
     },
     config = function()
-        require('telescope').setup({})
-        pcall(require('telescope').load_extension, 'fzf')
+        require("telescope").setup({})
+        pcall(require("telescope").load_extension, "fzf")
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
-        vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-        vim.keymap.set("n", "<space>/", builtin.current_buffer_fuzzy_find, { desc = '[/] Search in current buffer' })
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
+        vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+        vim.keymap.set("n", "<space>/", builtin.current_buffer_fuzzy_find, { desc = "[/] Search in current buffer" })
 
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-        vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
-        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-        vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-        vim.keymap.set('n', '<leader>fw',
-            function()
-                builtin.grep_string({ search = vim.fn.expand("<cword>") })
-            end,
-            { desc = '[F]ind [W]ord' })
-    end
+        vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+        vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Search [G]it [F]iles" })
+        vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+        vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+        vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+        vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+        vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+    end,
 }
